ac16ca85bd17ea4af410e3166b2681cb7d14d3a1
Zack <zack.c.campbell@gmail.com>
2018-11-29 20:42:23 -0600

    Finished Lab 10

--- Lab_10_Testing/src/TimeParserTest.java
+++ Lab_10_Testing/src/TimeParserTest.java
@@ -1,5 +1,6 @@
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +8,30 @@ public class TimeParserTest {
 
     @Test
     public void testFullStatementCoverage() {
-
+        assertEquals(43200, TimeParser.parseTimeToSeconds("0:00:00 pm"));
+        assertEquals(0, TimeParser.parseTimeToSeconds("0:00:00 am"));
     }
+
+    @Test
+    public void testFullBranchCoverage() {
+        assertEquals(82800, TimeParser.parseTimeToSeconds("11:00:00 pm"));
+        assertEquals(0, TimeParser.parseTimeToSeconds("0:00:00 cm"));
+    }
+
+    @Test
+    public void testFirstException() {
+        assertThrows(NumberFormatException.class, () -> TimeParser.parseTimeToSeconds("0 pm"));          // Test first exception that there isn't a colon
+    }
+
+    @Test
+    public void testSecondException() {
+        assertThrows(NumberFormatException.class, () -> TimeParser.parseTimeToSeconds("0:00 pm"));          // Test the second exception that there isn't a second colon
+    }
+
+    @Test
+    public void testThirdException() {
+        assertThrows(IllegalArgumentException.class, () -> TimeParser.parseTimeToSeconds("0:00:60 am"));   // Test the third exception that there isn't a valid time
+    }
+
+
 }
